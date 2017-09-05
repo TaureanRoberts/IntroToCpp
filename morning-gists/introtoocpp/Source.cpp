c36021b575ef648c1c8381a66881ff8f18506c8a
TaureanRoberts <taureanjroberts@gmail.com>
2017-09-05 10:02:26 -0500

    gists

---
+++ morning-gists/introtoocpp/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main()
+{
+
+	//Find the sum of the two largest values in an array
+	
+
+	
+
+	int a = 2;
+	int b = 3;
+	int sum = a + b;
+	{
+		
+		for (int a = 2; a == 2; a++);
+		std::cout << a << std::endl;
+
+		std::cout << "The second number is: \n";
+		for (int b = 3; b == 3; b++);
+		std::cout << b << std::endl;
+
+		std::cout << "The sum of the two numbers is: \n";
+		for (int sum = a + b; sum == 2 + 3; sum++);
+		std::cout << sum << std::endl;
+	}
+
+
+
+	
+	
+	system("pause");
+
+
+
+
+
+//	- Bonus allow the user to input the array values
+	int a;
+	int b;
+	int sum;
+
+	{
+       std::cout << "Choose one random number: \n";
+	   std::cin >> a >> "\n";
+	   for (int a = 0; a >=100000000; a++);
+	   std::cout << a << std::endl;
+
+	   std::cout << "Choose another number: \n";
+	   std::cin >> b >> "\n";
+	   for (int b = 0; b >= 100000000;);
+
+
+
+	}
+
+	system("pause");
+}
