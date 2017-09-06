a6669a45549cb407a8e30db0928fef53d347ce03
TaureanRoberts <taureanjroberts@gmail.com>
2017-09-06 09:18:10 -0500

    AssessmentOne

---
+++ AssessmentOne/HelloWorld/AssessmentOne.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int ByeWorld(int iCantStop, int yourWorld)
+{
+	int iCantStop;
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	std::cout << ByeWorld << std::endl;
+	system("pause");
+}
