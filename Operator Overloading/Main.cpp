2e9cf75516ec1a92d64286de5d72b757218b26c6
TaureanRoberts <taureanjroberts@gmail.com>
2017-09-27 08:17:21 -0500

    Couldnt find my player class

--- Operator Overloading/Main.cpp
+++ Operator Overloading/Main.cpp
@@ -7,8 +7,7 @@ int main()
 	Point2D testOne;
 	Point2D testTwo(0.0f, 1.0f);
 
-	Point2D testThree = testOne.GetX() + testOne.GetY(testTwo);
-	Point2D testFour = testThree.GetX(testTwo);
+	
 
 	system("pause");
 }
--- Operator Overloading/NewPoint2D.cpp
+++ Operator Overloading/NewPoint2D.cpp
@@ -75,8 +75,5 @@ int main()
 	Point2D testOne;
 	Point2D testTwo(0.0f, 1.0f);
 
-	Point2D testThree = testOne.GetX() + testOne.GetY(testTwo);
-	Point2D testFour = testThree.GetX(testTwo);
-
 	system("pause");
 }
