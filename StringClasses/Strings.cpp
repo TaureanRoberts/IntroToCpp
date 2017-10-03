eb36e11ba12b67727efe2aaa0aa24889b4e35311
TaureanRoberts <taureanjroberts@gmail.com>
2017-10-03 12:54:08 -0500

    strings class

--- StringClasses/Strings.cpp
+++ StringClasses/Strings.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include "Strings.h"
-
+/*
 MyString::MyString()
 {
 
 }
 
-MyString::MyString(int length, int height)
+MyString::MyString(int length)
 {
 
 }
@@ -32,3 +32,52 @@ MyString MyString::LowerString(MyString lhs)
 {
 	return MyString();
 }
+*/
+
+MyString::MyString()
+{
+
+}
+
+MyString::MyString(char *string)
+{
+	mString == string;
+}
+
+void MyString::GetLength()
+{
+	MyString myArray[89];
+
+}
+
+MyString MyString::ToUpper()
+{
+	char iter = 0;
+	char i = 65;
+	do
+	{
+
+		i += 2;
+		iter += 2;
+	} while (i <= 90);
+	return MyString();
+}
+
+MyString MyString::ToLower()
+{
+
+	return MyString();
+}
+
+bool MyString::SameString() //compares values in two instances of the string
+{
+	char someString = 0;
+	char equalString = 1;
+
+	do
+	{
+
+	}while();
+	return false;
+}
+
--- StringClasses/Strings.h
+++ StringClasses/Strings.h
@@ -5,12 +5,20 @@ class MyString
 private:
 	const char *mString;
 public:
+	/*
 	MyString();
-	MyString(int length, int height);
+	MyString(int length);
 	int mLenght();
-	void GetIndex();
+    void GetIndex();
 	MyString UpperString(MyString rhs);
 	MyString LowerString(MyString lhs);
-	
+	*/
 
+	MyString();
+	MyString(char *string);  
+    void GetLength();      //Returns the numbers of characters stored in the string
+	MyString ToUpper();    //Gives the string some uppercase letters
+	MyString ToLower();    //Gives the string some lowercase letters
+	bool SameString();     //Compares the strings together
+	
 };
