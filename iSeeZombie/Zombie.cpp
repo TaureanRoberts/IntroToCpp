aacbab52d342472cce1e2bdb682589347e207b29
Taurean Roberts <taureanjroberts@gmail.com>
2017-10-27 18:01:11 -0500

    .

--- iSeeZombie/Zombie.cpp
+++ iSeeZombie/Zombie.cpp
@@ -1,10 +1,20 @@
 #include <iostream>
 #include "Header.h"
-
+#include "time.h"
 int main()
-{
+{ 
+    srand(time(NULL));
 	Zombie zombieParty[5];
-
+	
+	
+	int zomDead = 0;
+	while (zomDead < 5)
+	{
+		zomDead = 0;
+		Zombie defendZom = zombieParty[rand() % 5];
+		Zombie attackZom = zombieParty[rand() % 5];
+		defendZom.TakeDamage(defendZom.GetHealth() - attackZom.GetAttack());
+	}
 
 		system("pause");
 }
